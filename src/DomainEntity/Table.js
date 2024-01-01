5f1267ade49c3f8f20e2668ff7e52aa5c37751d1
Nawal Arif Malik <romiarifmalik@gmail.com>
2024-01-01 13:21:57 +0500

    made  minor changes in Table_Enity

--- src/DomainEntity/Table.js
+++ src/DomainEntity/Table.js
@@ -6,40 +6,50 @@ class Table {
     validateName(){
         if(typeof this.name === "string"){
             return true;
+        }else{
+            return false;
         }
     }
 
     validateType(){
         if(typeof this.type === "string"){
             return true;
+        }else{
+            return false;
         }
     }
     
     validateProperties(){
         if(Array.isArray(this.properties)=== true){
             
-            var length=0;
-            for(let i=0; typeof this.properties[i]== "object"; i++){
-                length++;
+            var numOfObjects=0;
+            for(let i=0; typeof this.properties[i]== "object" && i < this.properties.length; i++){
+                numOfObjects++;
             }//looping through this.properties[] for object
 
-            var keep=this.#validateDatatypes(length)
-            return keep;
+            var funcReturnedVal=this.#validateDatatypes(numOfObjects)
+            return funcReturnedVal;
 
         }//if this.properties is an Array
+        else{
+            return false;
+        }
     }
 
-    #validateDatatypes(length){
-        if(length == this.properties.length){
-            var theObject;
-            for(let i=0; i< length; i++){
-                theObject=this.properties[i];
-                if(typeof theObject.name !== "string" || typeof theObject.type !== "string"){
-                    return undefined;
+    #validateDatatypes(numOfObjects){
+        if(numOfObjects ==0){
+            return false;
+        }else if(numOfObjects == this.properties.length){
+                    for(let i=0; i< numOfObjects; i++){
+                        if(typeof this.properties[i].name !== "string" || typeof this.properties[i].type !== "string"){
+                            return false;
+                        }
+                    }//looping through this.properties[] for dataTypes of keys in objects
+                    return true;
+                }//if variable named "numOfObjects" is equals to this.properties.length
+                else{
+                    return false;
                 }
-            }//looping through this.properties[] for dataTypes of keys in objects
-            return true;
-        }//if variable named "length" is equals to this.properties.length
     }
 
 }
