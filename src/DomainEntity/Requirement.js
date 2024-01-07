3ef1198bf5805c733a7f3f0652a5789ef2820fea
Nawal Arif Malik <romiarifmalik@gmail.com>
2024-01-08 01:21:22 +0500

    created Requirement-Entity

---
+++ src/DomainEntity/Requirement.js
@@ -0,0 +1,67 @@
+class Requirement{
+    code;
+    description;
+    #arrayForErrors=[]
+
+    validateCode(){
+        if(this.code==undefined){
+            this.#arrayForErrors.push("error: no value given to 'code'");
+        }
+        else if(typeof this.code != "string"){
+            this.#arrayForErrors.push("error: 'code' has invalid datatype")
+        }
+    }
+
+    validateDescription(){
+        if(this.description==undefined){
+            this.#arrayForErrors.push("error: no value given to 'description'")
+        }
+        else if(typeof this.description != "string"){
+            this.#arrayForErrors.push("error: 'description' has invalid datatype")
+        }
+    }
+
+    validate(){
+        this.validateCode();
+        this.validateDescription()
+        if(this.#arrayForErrors.length!=0){
+            return this.#arrayForErrors;
+        }
+    }
+
+    FuncForErrors(errorMessage){
+        this.#arrayForErrors.push(errorMessage);
+    }
+
+    returnArrayForErrors() {
+        return this.#arrayForErrors;
+    }
+    
+}
+
+class Non_Functional_Req extends Requirement{
+    type;
+
+    validateType(){
+        if(this.type ==undefined){
+            this.FuncForErrors("error: no value given to 'type'");
+        }
+        else if(typeof this.type != "string"){
+            this.FuncForErrors("error: 'type' has invalid datatype")
+        }
+    }
+
+    validate() {
+
+        super.validateCode();
+        super.validateDescription();
+        this.validateType();
+
+        if (this.returnArrayForErrors().length != 0) {
+            return this.returnArrayForErrors();
+        }
+    }
+
+}
+
+class Functional_Req extends Requirement{}
