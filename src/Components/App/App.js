97ec1d5e5d2fc87fe90edb7e9b5ccc2740e325c9
aliZaigham <alizaghamstudent@gmail.com>
2024-02-17 22:01:18 +0500

    SideBar fontawesome route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import "./App.css"
-import Container from "../Container/Container.js"
-import Table from "../Table/Table.js"
-import Form from "../Form/Form.js"
-import Detail from "../Detail/Detail.js"
-import NavBar from "../NavBar/NavBar.js"
-import Home from "../Home/Home.js"
+import NavBar from "../NavBar/NavBar.js";
+import SideBar from "../SideBar/sideBar.js";
 
-function App(){
-  return<div className="App">
-    <NavBar/>  
-    <Home/>
-  </div>
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <SideBar/>
+    </div>
+  );
 }
-
 export default App;
 
-
-//<Container component={<Form type={"product"}/>}/>
-//<Detail detail={{name:"SpreeSuite", description: "This is a product.", code:"12", preCondition: "abc", postCondition: "efg"}}/>
