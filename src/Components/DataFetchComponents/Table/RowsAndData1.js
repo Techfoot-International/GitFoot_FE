01c12ec839f5c8616c7fc3c8c581917d427b02ab
Nawal <romiarifmalik@gmail.com>
2024-02-23 22:36:09 +0500

    created storeInBrowser.js and made some changes to other files

--- src/Components/DataFetchComponents/Table/RowsAndData1.js
+++ src/Components/DataFetchComponents/Table/RowsAndData1.js
@@ -5,32 +5,17 @@ function RowsAndData(props) {
     var count =1;
     var arrTd = [];
     var arrTr = [];
-    
-    var Mapkeys = Object.keys(props.content.maping);
-    var Mapvalues=Object.values(props.content.maping)
-    var DataArr=props.content.data;
 
-    for (let x = 0; x < DataArr.length; x++) {
-        arrTd.push(<td className="th-td" key={`${x}-a`}>{count++}</td>)
-        for (let k = 0; k < props.content.heading.length; k++) {
-            for (let i = 0; i < props.content.heading.length; i++) {
-                if(props.content.heading[k].toUpperCase()===Mapkeys[i].toUpperCase()){
-                    if(DataArr[x][Mapvalues[i].toUpperCase()]!== undefined ){
-                        arrTd.push(<td key={`${x}-${k}-${i}`}>{DataArr[x][Mapvalues[i].toUpperCase()]}</td>)
-                        i=props.content.heading.length;
-                    }else{
-                        console.log("what")
-                        arrTd.push(<td key={`${x}-${k}-${i}`}>{DataArr[x][Mapvalues[i].toLowerCase()]}</td>)
-                        i=props.content.heading.length;
-                    }
-                    
-                }// if
-            }//for i < props.content.heading.length
-        }//k < props.content.heading.length
+    const product=props.content;
 
-        arrTr.push(<tr key={x}>{arrTd}</tr>)
-        arrTd=[]
-    }
+    for (let i = 0; i < props.content.length; i++) {
+        arrTd.push(<td className="th-td" key={`${i}-a`}>{count++}</td>);
+        arrTd.push(<td className="th-td" key={`${i}-b`}>{product[i].name}</td>)
+        arrTd.push(<td className="th-td" key={`${i}-c`}>{product[i].description}</td>)
+        
+        arrTr.push(<tr key={i}>{arrTd}</tr>)
+        arrTd=[]     
+    }//looping through product array
     return arrTr;
 }
     
